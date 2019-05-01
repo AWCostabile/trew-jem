2a12c320fd582e5bb2347532751dcae2b271636c
AWCostabile <jupiter.adept@gmail.com>
2019-05-02 03:04:41 +1000

    refactor: make server handle errors slightly differently

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -3,8 +3,9 @@ import { IServerRequest } from 'server/types/general';
 import * as WebServer from 'server/web';
 
 export const parseRequest = async (incoming: IServerRequest) => {
+  const { url } = incoming;
   try {
-    if (API_SERVER && incoming.url.match(new RegExp(`^${API_SERVER}`))) {
+    if (API_SERVER && url.match(new RegExp(`^${API_SERVER}`))) {
       const { data = {}, headers = {}, status = 200 } = await onRequest(
         incoming
       );
@@ -21,17 +22,19 @@ export const parseRequest = async (incoming: IServerRequest) => {
   } catch (err) {
     const status = err.status || 500;
 
+    const data = err.isApiError
+      ? { error: { code: err.code, reason: err.reason, status: err.status } }
+      : err;
+
     console.error(
-      `\x1b[31m[${status}]\x1b[0m Error handling request for '\x1b[36m${
-        incoming.route
-      }\x1b[0m'\n\x1b[31m\x1b[2m`,
-      err,
+      `\x1b[31m[${status}]\x1b[0m Error handling request for '\x1b[36m${url}\x1b[0m'\n\x1b[31m\x1b[2m`,
+      data,
       '\x1b[0m'
     );
 
     return {
       status,
-      data: err
+      data
     };
   }
 };
