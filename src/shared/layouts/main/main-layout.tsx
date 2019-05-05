4e54402270ea15c3c4259b2c7b4d6f9d512525f9
AWCostabile <jupiter.adept@gmail.com>
2019-05-06 01:29:47 +1000

    refactor: add router layout props typing to MainLayout

--- src/shared/layouts/main/main-layout.tsx
+++ src/shared/layouts/main/main-layout.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { IRouterLayout } from 'shared/types/routing';
 import { MainLayoutContainer } from './main-layout.emotion';
 
-export class MainLayout extends React.Component {
+export class MainLayout extends React.Component<IRouterLayout> {
   render() {
     const { children } = this.props;
 
