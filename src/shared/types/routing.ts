c0e8fe5c6522f7addba5e833bb983775c1723022
AWCostabile <jupiter.adept@gmail.com>
2019-05-06 01:28:33 +1000

    feat: add new types to give Layouts route info

--- src/shared/types/routing.ts
+++ src/shared/types/routing.ts
@@ -1,5 +1,19 @@
+import { Omit } from 'react-router';
+
 export interface IRoute {
   component: React.ComponentType;
+  disabled?: boolean;
+  icon?: React.ReactElement;
   exact?: boolean;
+  name?: string;
   path: string;
 }
+
+export interface INav extends Omit<IRoute, 'component'> {
+  isCurrent?: boolean;
+}
+
+export interface IRouterLayout {
+  navItems: INav[];
+}
+
